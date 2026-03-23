a2196cd3489dcadab4e56ad35b1649bc9fbb5232
Lewys Correa <lewys.correa@sofka.com.co>
2026-03-23 10:09:12 -0500

    Resolución taller semana 2

--- S2/main.py
+++ S2/main.py
@@ -12,6 +12,7 @@ intentos_maximos = 3
 intentos_actuales = 0
 autenticado = False
 rol_usuario = ""
+historial_chat = []
 
 
 while intentos_actuales < intentos_maximos and not autenticado:
@@ -49,6 +50,8 @@ while sistema_activo:
         sistema_activo = False
     elif cmd == "ping":
         print("pong.")
+        # Guardamos el recuerdo del comando ping ejecutado
+        historial_chat.append({"autor": rol_usuario, "descripcion": "El usuario ejecuto el comando ping"})
     elif cmd =="contar":
         palabra = input("Ingrese una palabra: ").lower()
         tot_letras = len(palabra)
@@ -65,20 +68,26 @@ while sistema_activo:
         print(f"Total de vocales: {tot_vocales}")
         print(f"Total de consonantes: {tot_cons}")
         print(f"Total de letras: {tot_letras}")
+        # Guardamos el recuerdo con la palabra usada
+        historial_chat.append({"autor": rol_usuario, "descripcion": f"Contó letras en la palabra: {palabra}"})
     elif cmd == "fecha_hoy":
         if rol_usuario == "invitado":
             print("[Acceso Denegado] Este comando requiere privilegios de administrador.")
+            historial_chat.append({"autor": rol_usuario, "descripcion": "Intento fallido de ver fecha (Acceso denegado)"})
             continue
 
         fecha_actual = datetime.now().strftime("%Y-%m-%d")
         print(f"Fecha actual: {fecha_actual}")
+        historial_chat.append({"autor": rol_usuario, "descripcion": f"Consultó la fecha actual: {fecha_actual}"})
 
     elif cmd == "validar_pass":
         nueva_pass = input("Ingrese la nueva contraseña: ")
         if len(nueva_pass) < 8 or nueva_pass == rol_usuario:
             print("La contraseña no cumple con las políticas de seguridad.")
+            historial_chat.append({"autor": rol_usuario, "descripcion": "Intento de cambio de pass inválido"})
         else:
             print("Contraseña válida. Se ha actualizado correctamente.")
+            historial_chat.append({"autor": rol_usuario, "descripcion": "Validó una nueva contraseña exitosamente"})
 
     elif cmd == "calculadora":
 #Se hace necesario convertir a float los valores de los input para poder permitir las operaciones matemáticas que se realizarán sobre ellos. 
@@ -103,6 +112,34 @@ while sistema_activo:
             continue
 
         print(f"Resultado: {resultado}")
+        historial_chat.append({"autor": rol_usuario, "descripcion": f"Realizó una operación: {num1} {operacion} {num2} = {resultado}"})
 
+    elif cmd.startswith("historial"):
+        partes = cmd.split() 
+        if len(partes) > 1 and partes[1] == "all":
+            if not historial_chat:
+                print("[PseudoAgente] La memoria está vacía.")
+            for memoria in historial_chat:
+                print(f"Autor: {memoria['autor']} | Acción: {memoria['descripcion']}")
+
+        elif len(partes) > 1 and partes[1] == "clear":
+            historial_chat.clear()
+            print("[PseudoAgente] Memoria borrada con éxito.")
+        else:
+            busqueda = input("Ingresa la palabra clave a buscar: ").lower()
+            coincidencias = 0
+            
+            for memoria in historial_chat:
+                descripcion_min = memoria["descripcion"].lower()
+                # Logré saber si una palabra estaba dentro de otra usando el operador in que busca dentro de otra adena en un string
+                # Resolví las singularidades del comando usando .split() para dividir la entrada en una lista y evaluar así su contenido
+                if busqueda in descripcion_min:
+                    print(f"-> Encontrado: [Autor: {memoria['autor']}] Mensaje: {memoria['descripcion']}")
+                    coincidencias += 1
+            
+            if coincidencias == 0:
+                print("[PseudoAgente] No encontré registros que coincidan con esa palabra.")
+            else:
+                print(f"[PseudoAgente] Se encontraron {coincidencias} coincidencias.")
     else:
         print("------Comando desconocido. Intente de nuevo.-------")
